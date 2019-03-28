6c97406cb607cc893dbf95608db4b47583bd7216
Noah Elkhatali <noahpc@live.dk>
2019-03-28 13:28:17 +0100

    Added materialize og jquery

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,7 +1,45 @@
+var myRec = new p5.SpeechRec('da'); // nyt speech rec objekt, som genkender danske ord
+
+let commands = [{
+		"rektor": ["rektor", "regtor", "actor"]
+	},
+	{
+		"kantine": ["kantine", "canteen"]
+	},
+];
+
+let resultDiv;
+
 function setup() {
-  // put setup code here
+	// default canvas setup
+	noCanvas();
+	// viser de(t) genkendte ord
+	myRec.continuous = true;
+	myRec.interimResults = true;
+	myRec.onResult = showResult;
+	myRec.onEnd = restartRec;
+	myRec.start();
+	$(".command-card").hide();
 }
 
-function draw() {
-  // put drawing code here
+function draw() {}
+
+// viser de(t) genkendte ord
+function showResult() {
+	if (myRec.resultValue == true) {
+		$("#speech").html(myRec.resultString);
+		for (e of commands) {
+			for (k of Object.values(e)[0]) {
+				if (myRec.resultString.includes(k)) {
+					console.log("Fandt ord: " + Object.keys(e));
+					divName = Object.keys(e);
+					$("#content").html($("#" + divName).clone().show());
+				}
+			}
+		}
+	}
+}
+
+function restartRec() {
+	myRec.start();
 }
