b7e18dba7c882769ba8d51140af306b99598f5d6
Noah Elkhatali <noahpc@live.dk>
2019-04-13 23:46:21 +0200

    Indentation

--- script.js
+++ script.js
@@ -4,72 +4,73 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 
 var recognition = new SpeechRecognition();
 
-let commands = [{
-		"rektor": ["rektor", "rejser", "actor"]
-	},
-	{
-		"studieadmin": ["studieadministration", "studieadministrationen"]
-	},
-	{
-		"vrlab": ["vr-lab", "vr lab", "vr lap", "vi er lab"]
-	},
-	{
-		"innolab": ["innolab", "inno lab"]
-	},
-	{
-		"f11t18": ["11", "12", "13", "14", "15", "16", "17", "18"]
-	},
-	{
-		"f21t25": ["21", "22", "23", "24", "25"]
-	},
-	{
-		"f31t32": ["31", "32"]
-	},
-	{
-		"f43t45": ["43", "44", "45"]
-	},
-	{
-		"f46t50": ["46", "47", "48", "49", "50"]
-	},
-	{
-		"f51t54": ["51", "52", "53", "54"]
-	},
-	{
-		"f60t63": ["60", "61", "62", "63"]
-	},
-	{
-		"proces": ["proces", "process", "prosa", "protest"]
-	},
-	{
-		"teori": ["teori"]
-	},
-	{
-		"biokemi": ["biokemi"]
-	},
-	{
-		"fysik": ["fysik"]
-	},
-	{
-		"printer": ["print", "printe", "printer", "printeren", "printere", "printerne"]
-	},
-	{
-		"bibliotek": ["bibliotek", "biblioteket"]
-	},
-	{
-		"teknologi": ["teknologi"]
-	},
-	{
-		"byg": ["byg", "Byg", "by"]
-	},
-	{
-		"musik": ["musik"]
-	},
-	{
-		"kantine": ["kantine", "sydsal", "syd sal"]
-	},
-	{
-		"nordsal": ["nordsal", "nord sal"]
-	}
+let commands = [
+    {
+        "rektor": ["rektor", "rejser", "actor"]
+    },
+    {
+        "studieadmin": ["studieadministration", "studieadministrationen"]
+    },
+    {
+        "vrlab": ["vr-lab", "vr lab", "vr lap", "vi er lab"]
+    },
+    {
+        "innolab": ["innolab", "inno lab"]
+    },
+    {
+        "f11t18": ["11", "12", "13", "14", "15", "16", "17", "18"]
+    },
+    {
+        "f21t25": ["21", "22", "23", "24", "25"]
+    },
+    {
+        "f31t32": ["31", "32"]
+    },
+    {
+        "f43t45": ["43", "44", "45"]
+    },
+    {
+        "f46t50": ["46", "47", "48", "49", "50"]
+    },
+    {
+        "f51t54": ["51", "52", "53", "54"]
+    },
+    {
+        "f60t63": ["60", "61", "62", "63"]
+    },
+    {
+        "proces": ["proces", "process", "prosa", "protest"]
+    },
+    {
+        "teori": ["teori"]
+    },
+    {
+        "biokemi": ["biokemi"]
+    },
+    {
+        "fysik": ["fysik"]
+    },
+    {
+        "printer": ["print", "printe", "printer", "printeren", "printere", "printerne"]
+    },
+    {
+        "bibliotek": ["bibliotek", "biblioteket"]
+    },
+    {
+        "teknologi": ["teknologi"]
+    },
+    {
+        "byg": ["byg", "Byg", "by"]
+    },
+    {
+        "musik": ["musik"]
+    },
+    {
+        "kantine": ["kantine", "sydsal", "syd sal"]
+    },
+    {
+        "nordsal": ["nordsal", "nord sal"]
+    }
 ];
 
 recognition.lang = 'da';
@@ -77,35 +78,36 @@ recognition.continuous = false;
 recognition.interimResults = false;
 
 function cardTimer() {
-	console.log("Timer starter...");
-	window.setTimeout(restartRec, 5000);
+    console.log("Timer starter...");
+    window.setTimeout(restartRec, 5000);
 }
 function restartRec() {
-	console.log("Lytter igen...");
-	recognition.start();
+    console.log("Lytter igen...");
+    recognition.start();
 }
 
 $(document).ready(function () {
-	recognition.start();
-	recognition.onsoundstart = function () {
-		console.log("Yeah, you heard me...");
-	}
-	recognition.onspeechend = function () {
-		console.log("Speech end.");
-		cardTimer();
-	}
-	$(".command-card").hide();
+    recognition.start();
+    recognition.onsoundstart = function () {
+        console.log("Yeah, you heard me...");
+    }
+    recognition.onspeechend = function () {
+        console.log("Speech end.");
+        cardTimer();
+    }
 
-	recognition.onresult = function (event) {
-		$("#speech").html(event.results[0][0].transcript);
-		for (e of commands) {
-			for (k of Object.values(e)[0]) {
-				if (event.results[0][0].transcript.includes(k)) {
-					console.log("Fandt ord: " + Object.keys(e));
-					divName = Object.keys(e);
-					$("#content").html($("#" + divName).clone().show());
-				}
-			}
-		}
-	}
+    $(".command-card").hide();
+
+    recognition.onresult = function (event) {
+        $("#speech").html(event.results[0][0].transcript);
+        for (e of commands) {
+            for (k of Object.values(e)[0]) {
+                if (event.results[0][0].transcript.includes(k)) {
+                    console.log("Fandt ord: " + Object.keys(e));
+                    divName = Object.keys(e);
+                    $("#content").html($("#" + divName).clone().show());
+                }
+            }
+        }
+    }
 });
